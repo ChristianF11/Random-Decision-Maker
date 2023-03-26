bc011fa0fe8343a317a68f36ef1ce70de09f3f6e
Christian <christianfresu02@gmail.com>
2023-03-26 18:53:45 +0200

    Inserimento "Moneta animata"
    Da questa versione al click dello schermo, il lancio della monetina avrà una sua animazione

--- lib/Moneta.dart
+++ lib/Moneta.dart
@@ -8,12 +8,13 @@ class LancioMoneta extends StatefulWidget {
   State<LancioMoneta> createState() => _LancioMonetaState();
 }
 
-class _LancioMonetaState extends State<LancioMoneta> with SingleTickerProviderStateMixin{
+class _LancioMonetaState extends State<LancioMoneta> with TickerProviderStateMixin{
 
-  //Controller con il compito di gestire l'animazione della moneta
-  late AnimationController _controller;
-  bool _risultato = true;
-  String _faccia = '';
+  late AnimationController _animationController;
+  late Animation<double> _animation;
+  late Animation<Offset> _fallingAnimation;
+  bool _activeAnimation = false;
+  bool _isHead = true;
 
   //Definizione del comportamento del controller
   @override
@@ -21,94 +22,141 @@ class _LancioMonetaState extends State<LancioMoneta> with SingleTickerProviderSt
 
     super.initState();
 
-    _controller = AnimationController(
-      duration: Duration(seconds: 2),
-      vsync: this,
-    )..repeat();
+    _animationController =
+    AnimationController(vsync: this, duration: const Duration(seconds: 0))
+      ..forward();
+
+    //Viene definito il tempo con cui si devono alternare le due facce
+    _animation = TweenSequence([
+      TweenSequenceItem(
+        tween:
+        Tween(begin: 0.0, end: 0.5).chain(CurveTween(curve: Curves.linear)),
+        weight: 0.375,
+      ),
+      TweenSequenceItem(
+        tween:
+        Tween(begin: 0.5, end: 1.0).chain(CurveTween(curve: Curves.linear)),
+        weight: 0.25,
+      ),
+      TweenSequenceItem(
+        tween:
+        Tween(begin: 1.0, end: 1.5).chain(CurveTween(curve: Curves.linear)),
+        weight: 0.375,
+      ),
+    ]).animate(_animationController);
+
+    //Viene definito il movimento verso la zona inferiore dello schermo
+    _fallingAnimation = Tween<Offset>(
+      begin: Offset.zero,
+      end: const Offset(0.0, 1.0),
+    ).animate(
+      CurvedAnimation(parent: _animationController, curve: Curves.easeIn),
+    );
+
+    //Viene deciso l'esito del lancio in maniera randomica
+    _animationController.addListener(() {
+      if (_animationController.status == AnimationStatus.forward ||
+          _animationController.status == AnimationStatus.reverse) {
+        if (_animationController.value < 1.5) {
+          setState(() {
+            _isHead = !_isHead;
+          });
+        } else {
+          setState(() {
+            _isHead = Random().nextBool();
+          });
+        }
+      }
+    });
+
+    //Al termine dell'animazione si ripartirà con la faccia appena uscita
+    _animationController.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        setState(() {
+          _isHead = _animation.value <= 0.5;
+        });
+      }
+    });
   }
 
   @override
   void dispose() {
 
-    _controller.dispose();
+    _animationController.dispose();
     super.dispose();
 
   }
 
-  //Funzioni
-  bool _lancio() {
-    bool _esito = Random().nextBool();
-    setState(() =>
-        _risultato = _esito); //Viene aggiornato lo 'State' di '_risultato'
+  //Funzione che serve per far alternare a livello grafico ed effettivo le due facce
+  bool _coinToss() {
+    setState(() {
+      _isHead = Random().nextBool();
+    });
 
-    if (_esito) {
-      _faccia = "testa";
-    } else {
-      _faccia = "croce";
-    }
-
-    return _esito;
+    return _isHead;
   }
 
-  void _showSnackBar() => ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-        content: Text('È uscita $_faccia'),
-        duration: const Duration(milliseconds: 1500),
-      ));
-
-  //Struttura
+  //Struttura applicazione
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.all(5),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          const Text(
-            'Premi il bottone per lanciare la monetina',
-            style: TextStyle(fontSize: 20.0),
-            textAlign: TextAlign.center,
-          ),
-          Visibility(
-            visible: _risultato,
-            child: SizedBox(
+      child: GestureDetector(
+        behavior: HitTestBehavior.opaque,
+        onTap: _activeAnimation
+            ? null //Se l'animazione è in corso cliccando lo schermo non succederà nulla
+            : () {
+          setState(() {
+            //La moneta è ferma e dunque vengono eseguite queste istruzioni
+            _animationController.reset();
+            _activeAnimation = true;
+
+            _animationController.duration = //_activeAnimation
+            //?
+            const Duration(seconds: 1);
+            //: const Duration(seconds: 0);
+          });
+
+          _animationController.forward().whenComplete(() {
+            setState(() {
+              _activeAnimation = false;
+            });
+
+            _animationController.stop();
+          });
+        },
+        child: Container(
+          child: Column(
+              children: [
+            const Text(
+              'Clicca lo schermo per lanciare la monetina',
+              style: TextStyle(fontSize: 20.0),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(
               height: 250,
-              width: 250,
-              child: Image.asset('assets/Moneta_testa.png'),
-            ),
-          ),
-          Visibility(
-            visible: !_risultato,
-            child: SizedBox(
-              height: 265,
-              width: 250,
-              child: Image.asset('assets/Moneta_croce.png'),
-            ),
-          ),
-          SizedBox(
-            height: 100,
-            width: 85,
-            child: ButtonTheme(
-              minWidth: 85,
-              height: 100,
-              child: Padding(
-                padding: const EdgeInsets.fromLTRB(0, 0, 0, 50),
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    primary: const Color(0xff448aff),
-                  ),
-                  onPressed: () {
-                    _lancio();
-                    _showSnackBar();
-                  },
-                  child: const Text('Lancio'),
+              child: SlideTransition(
+                position: _fallingAnimation,
+                child: Image.asset(
+                  _coinToss()
+                      ? 'assets/Moneta_testa.png'
+                      : 'assets/Moneta_croce.png',
+                  width: 300,
+                  height: 300,
                 ),
               ),
             ),
-          ),
-          const Text("Un'app di Christian Fresu",
-            style: TextStyle(fontSize: 12),
-          ),
-        ],
+                const Expanded(
+                  child: Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Text(
+                      "Un'app di Christian Fresu",
+                      style: TextStyle(fontSize: 12),
+                    ),
+                  ),
+                ),
+          ]),
+        ),
       ),
     );
   }
